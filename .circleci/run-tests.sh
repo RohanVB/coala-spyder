2d15c4a1ab6010f1d76d7458520797164f916fd4
RohanVB <rohan.vbh@gmail.com>
2019-06-20 18:38:42 -0400

    removes extra files

--- .circleci/run-tests.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -ex
-# -e means:  Exit immediately if a command exits with a non-zero status
-# -x means:  Print commands and their arguments as they are executed.
-
-source $HOME/miniconda/etc/profile.d/conda.sh
-conda activate test
-
-# Run tests; flag -x means stop on first test failure
-pytest -x -vv spyder_notebook --cov=spyder_notebook
